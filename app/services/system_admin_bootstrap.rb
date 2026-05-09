6929fa236ba9f926837e08fd714d16f8360363b2
jasonmag <jasonmaglangit@gmail.com>
2026-05-09 21:34:07 +0800

    updated deployment configuration

--- app/services/system_admin_bootstrap.rb
+++ app/services/system_admin_bootstrap.rb
@@ -2,13 +2,12 @@ class SystemAdminBootstrap
   def self.from_env!
     email = ENV["ADMIN_EMAIL"].to_s.strip.downcase
     password = ENV["ADMIN_PASSWORD"].to_s
-    name = ENV["ADMIN_NAME"].to_s.strip.presence || "System Admin"
 
     return nil if email.blank? || password.blank?
     return nil if password.length < 8
 
     user = User.find_or_initialize_by(email: email)
-    user.name = name
+    user.name = "System Admin"
     user.role = :system_admin
     user.password = password
     user.password_confirmation = password
--- test/services/system_admin_bootstrap_test.rb
+++ test/services/system_admin_bootstrap_test.rb
@@ -4,25 +4,22 @@ class SystemAdminBootstrapTest < ActiveSupport::TestCase
   setup do
     @previous_email = ENV["ADMIN_EMAIL"]
     @previous_password = ENV["ADMIN_PASSWORD"]
-    @previous_name = ENV["ADMIN_NAME"]
   end
 
   teardown do
     ENV["ADMIN_EMAIL"] = @previous_email
     ENV["ADMIN_PASSWORD"] = @previous_password
-    ENV["ADMIN_NAME"] = @previous_name
   end
 
   test "creates system admin when env is present" do
     ENV["ADMIN_EMAIL"] = "bootstrap-admin@example.com"
     ENV["ADMIN_PASSWORD"] = "supersecret123"
-    ENV["ADMIN_NAME"] = "Bootstrap Admin"
 
     user = SystemAdminBootstrap.from_env!
 
     assert user.persisted?
     assert_equal "bootstrap-admin@example.com", user.email
-    assert_equal "Bootstrap Admin", user.name
+    assert_equal "System Admin", user.name
     assert_equal "system_admin", user.role
   end
 
