1de7066d3256696bc71fdb5d028c7c7d8cc8151e
jasonmag <jasonmaglangit@gmail.com>
2026-05-09 15:21:13 +0800

    Allow assets precompile without API token; keep runtime enforcement

--- config/initializers/api_security.rb
+++ config/initializers/api_security.rb
@@ -1,5 +1,10 @@
 if Rails.env.in?(%w[production staging])
-  if ENV["API_V1_TOKEN"].to_s.blank?
+  running_assets_precompile =
+    defined?(Rake) &&
+    Rake.respond_to?(:application) &&
+    Rake.application.top_level_tasks.any? { |task| task.start_with?("assets:precompile") }
+
+  if ENV["API_V1_TOKEN"].to_s.blank? && !running_assets_precompile
     raise "API_V1_TOKEN must be set in #{Rails.env} for /api/v1 authentication"
   end
 end
